2718937377c4a625a6e1fe464e7844457e897dba
Asher Cohen <ashercohen@berkeley.edu>
2021-11-03 16:54:55 -0700

    Spaced out matrices, added CNOT gate

--- Dictionary, Common QGates.py
+++ Dictionary, Common QGates.py
@@ -1,6 +1,16 @@
 import numpy as np
 
-H = 0.5**0.5 * np.array([[1, 1],[1, -1]]) #Hadamard Gate
+#Hadamard Gate
+H = 0.5**0.5 * np.array([[1, 1],\
+                         [1, -1]]) 
 
-CNOT = np.array([[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]])
+#NOT Gate
+NOT = np.array([[0,1],\
+                [1,0]])
+
+#Controlled NOT Gate
+CNOT = np.array([[1,0,0,0],\
+                 [0,1,0,0],\
+                 [0,0,0,1],\
+                 [0,0,1,0]])
   
