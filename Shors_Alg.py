755e2a9896b4b4c5e8a2ff43c4bcabbef1ff9875
DylanM <Dylanjmengell@gmail.com>
2021-11-16 19:38:49 -0800

    adjusted Shor's algo to be in func

--- Shors_Alg.py
+++ Shors_Alg.py
@@ -1,70 +1,76 @@
 #Shors Alg. Classical part
 import numpy as np
 
-#Put the Composite Number Here
-N = int(input('Number to Factor (Cannot be even): '))
-if (N % 2) == 0:
-   print('The value is even please enter an odd N!')
-   N = input('Number to Factor (Cannot be even): ')
 
-#1 Pick a random number 1<a<N
-#non_trivial_divisor = np.random.randint(0,N,1)  
-non_trivial_divisor = int(input('Number to factor 2:'))
+def QuantPeriodFinding(): #quantum Period finding algorthm Should return r
+    """
+    def part4(X,Y): #(Non_trivial_divisor, N)
+        p = np.linspace(1,Y)
+        for j in p:
+            m = np.linspace(1,Y)
+            for l in m:
+                #remember j and l are m and p
+                if ((X**j) == (l * Y) + 1): #ERROR HERE
+                    return (j,l)
 
-#Compute K = GCD(a,N) using Euclidean Algorithm 
+    p,m = part4(non_trivial_divisor, N)
+    #goal find a better a
+    def findBettera (p,m,N):
+        a = ((m * N) + 1)**p
+        return a
 
-def euclideanAlg(a,N):
+    a = findBettera(p,m,N)
+    print(a)
+    """
+    return 0
+
+def euclideanAlg(a,N): #Euclidean algorithm for finding GCD
     if (a==0):
         return N
     else:
-        return euclideanAlg2(N % a, a)
+        return euclideanAlg(N % a, a)
 
-#works!
-K = euclideanAlg(non_trivial_divisor, N)
+#Put the Composite Number Here
+def ShorsAlgo(N):
+    isdone = True
+    while not (isdone):
+        if (N % 2) == 0:
+            return ('The value is even please enter an odd N!')
 
-#3
-if K != 1:
-    non_trivial_factor = K #WE DID IT, no quantum needed
-    print('Your GCD is: ', non_trivial_factor)
-elif K == 1:
-    print('The values are Coprime. Your GCD is 1, therefore we must continue to step 4.')
+        #1 Pick a random number 1<a<N
+        non_trivial_divisor = np.random.randint(1,N,1)  #possibly find better random num gen
 
-#4 Use the quantum period-finding subroutine to find r
-#(non_trivial_divisor)**p = m * N +1
+        #Compute K = GCD(a,N) using Euclidean Algorithm 
+        #works!
+        K = euclideanAlg(non_trivial_divisor, N)
 
-#Need to create a way to test out an infinite number of values 
-#for p and m, including fractions
+        #3
+        if K != 1:
+            non_trivial_factor = K #WE DID IT, no quantum needed
+            print('Your GCD is: ', non_trivial_factor)
+        elif K == 1:
+            print('The values are Coprime. Your GCD is 1, therefore we must continue to step 4.')
 
-#leave this up to quauntum computing
-"""
-def part4(X,Y): #(Non_trivial_divisor, N)
-    p = np.linspace(1,Y)
-    for j in p:
-        m = np.linspace(1,Y)
-        for l in m:
-            #remember j and l are m and p
-            if ((X**j) == (l * Y) + 1): #ERROR HERE
-                return (j,l)
+        #4 Use the quantum period-finding subroutine to find r
+        #(non_trivial_divisor)**p = m * N +1
 
-p,m = part4(non_trivial_divisor, N)
-#goal find a better a
-def findBettera (p,m,N):
-    a = ((m * N) + 1)**p
-    return a
+        #Need to create a way to test out an infinite number of values 
+        #for p and m, including fractions
 
-a = findBettera(p,m,N)
-print(a)
-"""
+        #leave this up to quauntum computing
 
-#5 if r is odd, go back to step 1
-if (r % 2) == 0:
-    non_trivial_divisor = np.random.randint(0,N,1) 
-#6 if a^(r/2) = -1(mod(N)) go back to step 1
+        #5 if r is odd, go back to step 1
+        if (r % 2) == 0:
+            non_trivial_divisor = np.random.randint(0,N,1) 
+        #6 if a^(r/2) = -1(mod(N)) go back to step 1
 
-# 7 other wise 
-# K1 = euclideanAlg((a**(r/2) + 1, N)
-# and 
-# K2 = euclideanAlg((a**(r/2) - 1, N)
-#^^ these are the non_trivial_factors of N
-    
-#goal = find non_trivial_factor
+        # 7 other wise 
+        # K1 = euclideanAlg((a**(r/2) + 1, N)
+        # and 
+        # K2 = euclideanAlg((a**(r/2) - 1, N)
+        #^^ these are the non_trivial_factors of N
+            
+        #goal = find non_trivial_factor
+
+
+        #==============add parts 5 and 6 to end inf loop
